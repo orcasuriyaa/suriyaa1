4416a08afdca3a76dde53f671cf1e37cbd9453e4
orcasuriyaa <ablazesuriyaa7@gmail.com>
2014-08-26 12:08:53 +0530

    C4Exercise6

---
+++ C4Exercise6.c
@@ -0,0 +1,18 @@
+/* Program to Evaluate a polynomial 
+   SuriyaaKumar G M , Aug25 2014   */
+
+#include<stdio.h>
+
+int main(void)
+{
+
+	int x = 2.55;
+	int s;
+
+	s = 3 * x ^ 3 - 5 * x ^ 2 + 6;
+
+	printf("the value of the polynomial is %i \n", s);
+
+	return 0;
+}
+
