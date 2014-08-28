973a6bfb769e1260f04d3c864e61be7c8897c843
orcasuriyaa <ablazesuriyaa7@gmail.com>
2014-08-28 09:58:21 +0530

    Dir

---
+++ Chapter 5/C5Example6.c
@@ -0,0 +1,26 @@
+/* Program Using While Loop
+   SuriyaaKumar G M, Aug27 2014 */
+
+#include<stdio.h>
+
+int main(void)
+	{
+
+	int count = 1;
+	
+	while(count <= 5)
+	
+	{
+	
+	printf("%i\n", count);
+	++count;
+	
+	}
+	
+	return 0;
+
+}
+
+
+
+
