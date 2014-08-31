83c2576ebe6c084508aabb0fd5ee5e4003555762
orcasuriyaa <ablazesuriyaa7@gmail.com>
2014-08-31 18:52:41 +0530

    Dir

---
+++ Chapter 8/Example2.c
@@ -0,0 +1,18 @@
+/* Calling A Function
+   SuriyaaKumar G M, Aug31 2014 */
+
+#include <stdio.h>
+
+void printMessage (void)
+{
+	printf ("Programming is fun.\n");
+}
+
+int main(void)
+{
+	printMessage ();
+	printMessage ();
+
+	return 0;
+}
+
