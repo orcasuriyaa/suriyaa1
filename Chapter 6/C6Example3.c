2a40022424497d84274be7f336f8e06eee5efa07
orcasuriyaa <ablazesuriyaa7@gmail.com>
2014-08-31 08:39:04 +0530

    Dir

---
+++ Chapter 6/C6Example3.c
@@ -0,0 +1,28 @@
+/* Program to find Whether no is odd or even
+   SuriyaaKumar G M, Aug30 2014 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int number_to_test;
+	int remainder;
+
+	printf("Enter the the number to test :\n");
+	scanf("%d", &number_to_test);
+
+	remainder = number_to_test % 2;
+
+	if(remainder == 0)
+	{
+		printf("The number is even");
+	}
+	else if(remainder != 0)
+	{
+		printf("The number is odd");
+	}
+
+	return 0;
+}
+
+
