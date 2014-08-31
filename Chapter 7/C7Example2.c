0df922baec1966a068c21c8785813169987057b1
orcasuriyaa <ablazesuriyaa7@gmail.com>
2014-08-31 10:48:20 +0530

    Dir

---
+++ Chapter 7/C7Example2.c
@@ -0,0 +1,30 @@
+/* Program to demonstrate an Array of counters
+   SuriyaaKumar G M, Aug 31 2014 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	int ratingCounters[11];
+ 	int i;
+	int response;
+
+	for ( i = 1; i <= 10; ++i )
+	ratingCounters[i] = 0;
+	
+	printf ("Enter your responses\n");
+	for ( i = 1; i <= 20; ++i ) {
+	scanf ("%i", &response);
+	
+	if ( response < 1 || response > 10 )
+	printf ("Bad response: %i\n", response);
+	else
+	++ratingCounters[response];
+}
+
+	printf ("\n\nRating Number of Responses\n");
+	printf ("------ -------------------\n");
+	for ( i = 1; i <= 10; ++i )
+	printf ("%4i%14i\n", i, ratingCounters[i]);
+	return 0;
+}
