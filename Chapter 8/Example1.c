d35f4ba9f9cd60e142a7844647dba0de46c7d646
orcasuriyaa <ablazesuriyaa7@gmail.com>
2014-08-31 18:40:22 +0530

    Dir

---
+++ Chapter 8/Example1.c
@@ -0,0 +1,16 @@
+/* Writing a Function in C
+   SuriyaaKumar G M, Aug31 2014 */
+
+#include <stdio.h>
+
+void printMessage(void)
+{
+	printf ("Programming is fun.\n");
+}
+
+int main (void)
+{
+	printMessage ();
+	
+	return 0;
+}
